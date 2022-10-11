e79191b5156647f5d2cce7af824ae5c5114040ef
Nahyun Kim <nahyun@sookmyung.ac.kr>
2022-10-11 16:46:51 +0900

    06_02

--- main.c
+++ main.c
@@ -3,18 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int print_star() 
-{
-	int i;
-	for (i=0;i<10;i++)
-		printf("*");
-		
+
+void func (int x){
+	
+	printf("func x is at %p\n", &x);
 }
 int main(int argc, char *argv[]) {
 	
-	print_star();
-	print_star();
-	print_star();
+	int x;
+	printf("main x is at %p\n", &x);
+	func(x);
+	
 	
 	return 0;
 }
