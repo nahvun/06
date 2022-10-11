4c6423b4aab332ecb2cd685ac7feb06b556b12b5
Nahyun Kim <nahyun@sookmyung.ac.kr>
2022-10-11 17:06:23 +0900

    06_04

--- main.c
+++ main.c
@@ -3,33 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a, int b)
+int square(int a)
 {
-	return a+b;
-}
-
-int square(int n)
-{
-	return (n*n);
-}
-
-int get_max(int x, int y)
-{
-	if (x > y)
-		return x;
-	else
-		return y;
+	return(a*a);
 }
 
 int main(int argc, char *argv[]) {
 	
-	int res_sumTwo, res_square, res_get_max;
+	int a = 2;
+	a = square(a);
+	printf("a = %i\n", a);
 	
-	res_sumTwo = sumTwo(2, 3);
-	res_square = square(5);
-	res_get_max = get_max(10, -20);
-	
-	printf("%i %i %i", res_sumTwo, res_square, res_get_max);
-
 	return 0;
 }
