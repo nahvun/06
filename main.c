5f65dbd76758e5b9b671476c5a97f3fb8eafc290
Nahyun Kim <nahyun@sookmyung.ac.kr>
2022-10-11 17:38:23 +0900

    06_05

--- main.c
+++ main.c
@@ -3,16 +3,34 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a)
+int combination(int n, int r)
 {
-	return(a*a);
-}
-
+	int a, b, c;
+	
+	a = factorial(n);
+	b = factorial(n-r);
+	c = factorial(r);
+	return(a/(b*c));
+ } 
+ 
+int factorial(int x)
+ {
+ 	int i, res = 1;
+ 	
+ 	for (i=1; i <= x; i++)
+ 		res = res*i;
+ 		
+ 	return res;
+ }
+ 
 int main(int argc, char *argv[]) {
+	int n, r, result;
 	
-	int a = 2;
-	a = square(a);
-	printf("a = %i\n", a);
 	
-	return 0;
+	printf("정수 두 개를 입력하세요 : ");
+	scanf("%d %d", &n, &r);
+	
+	result = combination(n, r);
+	printf("result is : %d", result);
 }
+	
